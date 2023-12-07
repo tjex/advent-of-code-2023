8c7b6874afa80b5838e1b329433aeec9743ee9da
tjex <tjex@tjex.net>
2023-12-07 15:59:58 +0100

    ok data handling structure designed

--- 03/main.go
+++ 03/main.go
@@ -14,36 +14,8 @@ func main() {
 	inputData := extractInputData()
 	symbolIndicies := extractSymbolIndicies(inputData)
 	numberIndicies := extractNumberIndicies(inputData)
-    for i := 0 ; i < len(numberIndicies); i++ {
-		fmt.Println("number:", i, numberIndicies[i])
-        fmt.Println("symbol", i, symbolIndicies[i])
-        fmt.Println()
-	}
+	checkNumberBoundaries(inputData, symbolIndicies, numberIndicies)
 }
-func extractNumberIndicies(inputData []string) [][][]int {
-	var numbers [][][]int
-	symbol := regexp.MustCompile(numberPat)
-	for _, line := range inputData {
-		found := symbol.FindAllStringIndex(line, -1)
-		numbers = append(numbers, found)
-	}
-
-	return numbers
-
-}
-
-func extractSymbolIndicies(inputData []string) [][][]int {
-	var symbols [][][]int
-	symbol := regexp.MustCompile(symbolPat)
-	for _, line := range inputData {
-		found := symbol.FindAllStringIndex(line, -1)
-		symbols = append(symbols, found)
-	}
-
-	return symbols
-
-}
-
 func extractInputData() []string {
 	var inputDataAsStringArray []string
 	file, err := os.Open("input.txt")
@@ -58,4 +30,74 @@ func extractInputData() []string {
 	}
 
 	return inputDataAsStringArray
+}
+
+func extractNumberIndicies(inputData []string) [][][]int {
+	var numbers [][][]int
+	symbol := regexp.MustCompile(numberPat)
+	for _, line := range inputData {
+		found := symbol.FindAllStringIndex(line, -1)
+		numbers = append(numbers, found)
+	}
+	numbers = numbers[:]
+
+	return numbers
+}
+
+func extractSymbolIndicies(inputData []string) [][][]int {
+	var symbols [][][]int
+	symbol := regexp.MustCompile(symbolPat)
+	for _, line := range inputData {
+		found := symbol.FindAllStringIndex(line, -1)
+		symbols = append(symbols, found)
+	}
+
+	return symbols
+
+}
+
+func convertTo2DArray(a [][][]int) [][]int {
+	var twoDarray [][]int
+	for _, line := range a {
+		for _, row := range line {
+			twoDarray = append(twoDarray, row)
+		}
+	}
+
+	return twoDarray
+
+}
+
+func printDataAtRange(row, rangeLeft, rangeRight int, inputData []string) {
+	for i, line := range inputData {
+		if i == row {
+			fmt.Println(line[rangeLeft:rangeRight])
+		}
+
+	}
+
+}
+
+// check if symbol exists in any neighbouring cell to a number, and if so return the numer
+func checkNumberBoundaries(inputData []string, symData, numData [][][]int) {
+	// dataRows := len(numData)
+	// data arrays with different nRows will break this function
+	if (len(symData) - len(numData)) != 0 {
+		panic(`symbol and number indicies array length mismatch,
+        something has gone wrong when extracting indicies for each type`)
+	}
+
+	// for each nIndicie group, check if:
+	// - a symbol exists (+/- 1 cell in the same row)
+	// - a symbol exists (within the same range OR +/- 1 cell in rows +/- 1)
+
+	for i, line := range numData {
+		for _, row := range line {
+			rangeLeft := row[0]
+			rangeRight := row[1]
+			fmt.Println(rangeLeft, rangeRight)
+			printDataAtRange(i, rangeLeft, rangeRight, inputData)
+		}
+	}
+
 }
